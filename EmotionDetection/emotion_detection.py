72345cf308c02a03962a3653f0a2b25a673e5a47
adhilchalil <chaliladhil@gmail.com>
2025-03-06 01:17:40 -0500

    project completed

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,16 @@ def emotion_detector(text_to_analyse):
     headers={"grpc-metadata-mm-model-id":   "emotion_aggregated-workflow_lang_en_stock"}
     json_obj={ "raw_document": { "text": text_to_analyse } }
     response = requests.post(URL, json = json_obj, headers=headers)
+    if response.status_code == 400:
+        return  {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+         
     formatted_response = json.loads(response.text)
     emotion_scores = formatted_response["emotionPredictions"][0]["emotion"]
     dominant_emotion = 'anger'
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+'''Module to run a Web app handling text emotion analyzing'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,17 +6,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    ''' Renders the base page '''
     return render_template("index.html")
 
 @app.route("/emotionDetector")
-def emotion_detector():
+def emotion_detector_api():
+    ''' Takes in a text string and returns emotions by
+    calling Watson API for text emotion analyzing'''
     text_to_analyze = request.args.get("textToAnalyze")
-    if not text_to_analyze:
-        return "Uhm.. Say something!"
-
     emotions = emotion_detector(text_to_analyze)
-    
-    return f"For the given statement, the system response is anger: {emotions['anger']}, disgust: {emotions['disgust']}, fear: {emotions['fear']}, joy: {emotions['joy']} and sadness: {emotions['sadness']} The dominant emotion is joy."
+    if emotions['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
+    return f"For the given statement, the system response is anger: {emotions['anger']},-\
+     disgust: {emotions['disgust']}, fear: {emotions['fear']}, joy: {emotions['joy']}-\
+      and sadness: {emotions['sadness']} The dominant emotion is joy."
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
